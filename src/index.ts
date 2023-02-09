bf4c47491a6a2aecc9ea45e7262c7ad42aecba90
Pivithuru Jayakody <pivithurujayakody@gmail.com>
2023-02-09 15:16:01 +0530

    Refactor KeygenService

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 #! /usr/bin/env node
 
-import figlet from "figlet";
 import { Command } from "commander";
-import { PrintService } from "./services/print";
+import figlet from "figlet";
 import * as fs from "fs";
-import { join } from "path";
 import { sign, verify } from "jsonwebtoken";
-import { exec } from "child_process";
+import { join } from "path";
+import { KeygenService } from "./services/keygen";
+import { PrintService } from "./services/print";
 
 const APP_NAME = "RSA Passphrase";
 const APP_VERSION = "1.0.0";
@@ -32,21 +32,7 @@ program
   .option("-b, --bitSize [size]", "Bit size of the key", "2048")
   .action((options) => {
     let bitSize = parseInt(options.bitSize) || 2048;
-    let generatePrivateKeyCommand = "openssl genrsa -out ./certs/private.pem";
-
-    if (bitSize && bitSize % 2 == 0) {
-      generatePrivateKeyCommand = generatePrivateKeyCommand + ` ${bitSize}`;
-    }
-
-    console.info(`⏳ Generating private key of size ${bitSize} bits`);
-    exec(generatePrivateKeyCommand, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`error: ${error.message}`);
-        return;
-      }
-      console.info(`⌛ ${stdout || stderr}`);
-      console.log("✅ Done!");
-    });
+    KeygenService.generatePrivateKey(bitSize);
   });
 
 program
@@ -55,26 +41,7 @@ program
   .option("-p, --passphrase [passphrase]", "Use passphrase")
   .action((options) => {
     let passphrase = options.passphrase;
-    let generatePublicKeyCommand =
-      "openssl rsa -in ./certs/private.pem -pubout -out ./certs/public.pem";
-
-    if (passphrase)
-      generatePublicKeyCommand =
-        generatePublicKeyCommand + ` -passout pass:${passphrase}`;
-
-    console.info(
-      `⏳ Generating public key${
-        !passphrase ? "" : ` with "${passphrase}" as the passphrase`
-      }`
-    );
-    exec(generatePublicKeyCommand, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`error: ${error.message}`);
-        return;
-      }
-      console.info(`⌛ ${stdout || stderr}`);
-      console.log("✅ Done!");
-    });
+    KeygenService.generatePublicKey(passphrase);
   });
 
 program
---
+++ src/services/keygen.ts
@@ -0,0 +1,60 @@
+import { exec } from "child_process";
+import * as fs from "fs";
+
+export class KeygenService {
+  static makeCertsDirectory() {
+    try {
+      fs.mkdirSync("./certs");
+    } catch (error) {
+      if ((error as any)?.code !== "EEXIST") {
+        console.error(error);
+        throw error;
+      }
+    }
+  }
+
+  static generatePrivateKey(bitSize: number) {
+    this.makeCertsDirectory();
+
+    let generatePrivateKeyCommand = "openssl genrsa -out ./certs/private.pem";
+
+    if (bitSize && bitSize % 2 == 0) {
+      generatePrivateKeyCommand = generatePrivateKeyCommand + ` ${bitSize}`;
+    }
+
+    console.info(`⏳ Generating private key of size ${bitSize} bits`);
+    exec(generatePrivateKeyCommand, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`error: ${error.message}`);
+        return;
+      }
+      console.info(`⌛ ${stdout || stderr}`);
+      console.log("✅ Done!");
+    });
+  }
+
+  static generatePublicKey(passphrase: string) {
+    this.makeCertsDirectory();
+
+    let generatePublicKeyCommand =
+      "openssl rsa -in ./certs/private.pem -pubout -out ./certs/public.pem";
+
+    if (passphrase)
+      generatePublicKeyCommand =
+        generatePublicKeyCommand + ` -passout pass:${passphrase}`;
+
+    console.info(
+      `⏳ Generating public key${
+        !passphrase ? "" : ` with "${passphrase}" as the passphrase`
+      }`
+    );
+    exec(generatePublicKeyCommand, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`error: ${error.message}`);
+        return;
+      }
+      console.info(`⌛ ${stdout || stderr}`);
+      console.log("✅ Done!");
+    });
+  }
+}
