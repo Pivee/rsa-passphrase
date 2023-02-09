45d3603a48000c8559ca7cf74cc777c8f2441f7f
Pivithuru Jayakody <pivithurujayakody@gmail.com>
2023-02-09 12:05:06 +0530

    Configure `commander`

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 import figlet from "figlet";
+import { Command } from "commander";
+
+const program = new Command()
+  .version("1.0.0")
+  .description("Testing tool for RSA Passphrase Utilization")
+  .option("-g, --generate", "Generate new RSA Private Key")
+  .parse(process.argv);
 
 console.log(figlet.textSync("RSA Passphrase", { font: "Small" }));
