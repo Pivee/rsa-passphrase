b31f24de5fa5bcea74ad3254d6b23a822e8556b6
Pivithuru Jayakody <pivithurujayakody@gmail.com>
2023-02-09 15:06:32 +0530

    Create key generation methods

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { PrintService } from "./services/print";
 import * as fs from "fs";
 import { join } from "path";
 import { sign, verify } from "jsonwebtoken";
+import { exec } from "child_process";
 
 const APP_NAME = "RSA Passphrase";
 const APP_VERSION = "1.0.0";
@@ -23,6 +24,57 @@ program
   .option("-m, --message [message]", "Print a message", "Hello World!")
   .action((options) => {
     if (options.message) PrintService.printToConsole(options.message);
+  });
+
+program
+  .command("generate-private")
+  .description("Generate RSA private key")
+  .option("-b, --bitSize [size]", "Bit size of the key", "2048")
+  .action((options) => {
+    let bitSize = parseInt(options.bitSize) || 2048;
+    let generatePrivateKeyCommand = "openssl genrsa -out ./certs/private.pem";
+
+    if (bitSize && bitSize % 2 == 0) {
+      generatePrivateKeyCommand = generatePrivateKeyCommand + ` ${bitSize}`;
+    }
+
+    console.info(`⏳ Generating private key of size ${bitSize} bits`);
+    exec(generatePrivateKeyCommand, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`error: ${error.message}`);
+        return;
+      }
+      console.info(`⌛ ${stdout || stderr}`);
+      console.log("✅ Done!");
+    });
+  });
+
+program
+  .command("generate-public")
+  .description("Generate RSA public key")
+  .option("-p, --passphrase [passphrase]", "Use passphrase")
+  .action((options) => {
+    let passphrase = options.passphrase;
+    let generatePublicKeyCommand =
+      "openssl rsa -in ./certs/private.pem -pubout -out ./certs/public.pem";
+
+    if (passphrase)
+      generatePublicKeyCommand =
+        generatePublicKeyCommand + ` -passout pass:${passphrase}`;
+
+    console.info(
+      `⏳ Generating public key${
+        !passphrase ? "" : ` with "${passphrase}" as the passphrase`
+      }`
+    );
+    exec(generatePublicKeyCommand, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`error: ${error.message}`);
+        return;
+      }
+      console.info(`⌛ ${stdout || stderr}`);
+      console.log("✅ Done!");
+    });
   });
 
 program
